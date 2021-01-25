cd5829aa3d2d3d40e68d375fceddf6f1f2e1e38d
deeplythink <hu928973510@gmail.com>
2021-01-25 15:18:45 +0800

    auto

--- src/models/DocModel/model.go
+++ src/models/DocModel/model.go
@@ -1,5 +1,7 @@
 package DocModel
 
+import "fmt"
+
 type DocImpl struct {
 	DocID       int           `gorm:"column:doc_id;primary_key"  json:"doc_id"`
 	DocTitle    string        `gorm:"column:doc_title" json:"label"`
@@ -13,4 +15,12 @@ type DocGrpImpl struct {
 	GroupName     string        `gorm:"column:group_name" json:"label"`
 	GroupShortUrl string        `gorm:"column:shorturl" json:"url"`
 	Children      []interface{} `gorm:"-" json:"children,omitempty"`
+}
+
+func (this *DocImpl) String() string {
+	return fmt.Sprintf("DocID:%d,DocTitle:%s,DocShortUrl:%s,DocHref:%s", this.DocID, this.DocTitle, this.DocShortUrl, this.DocHref)
+}
+
+func (this *DocGrpImpl) String() string {
+	return fmt.Sprintf("GroupID:%d,GroupName:%s,GroupShortUrl:%s", this.GroupID, this.GroupName, this.GroupShortUrl)
 }
