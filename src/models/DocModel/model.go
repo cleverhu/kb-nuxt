d462c968ebce5090cb0b5d18db456e4132e1ff93
deeplythink <hu928973510@gmail.com>
2021-01-25 15:10:55 +0800

    auto

--- src/models/DocModel/model.go
+++ src/models/DocModel/model.go
@@ -1,20 +1,16 @@
 package DocModel
 
 type DocImpl struct {
-	DocID       int    `gorm:"column:doc_id;primary_key"  json:"id"`
-	DocTitle    string `gorm:"column:doc_title" json:"label"`
-	DocShortUrl string `gorm:"column:shorturl" json:"url"`
-	DocHref     string `gorm:"-"` //拼接地址字符串
-	Children  []interface{}       `gorm:"-" json:"children,omitempty"`
+	DocID       int           `gorm:"column:doc_id;primary_key"  json:"doc_id"`
+	DocTitle    string        `gorm:"column:doc_title" json:"label"`
+	DocShortUrl string        `gorm:"column:shorturl" json:"url"`
+	DocHref     string        `gorm:"-"` //拼接地址字符串
+	Children    []interface{} `gorm:"-" json:"children,omitempty"`
 }
 
 type DocGrpImpl struct {
-	GroupID       int                 `gorm:"column:group_id;primary_key" json:"-"`
-	GroupName     string              `gorm:"column:group_name" json:"label"`
-	GroupShortUrl string              `gorm:"column:shorturl" json:"url"`
+	GroupID       int           `gorm:"column:group_id;primary_key" json:"group_id"`
+	GroupName     string        `gorm:"column:group_name" json:"label"`
+	GroupShortUrl string        `gorm:"column:shorturl" json:"url"`
 	Children      []interface{} `gorm:"-" json:"children,omitempty"`
 }
-
-
-
-
