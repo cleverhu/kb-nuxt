cfce57b9cbd207aa56f281adf106ba0390df714f
h31 <h31@h31deiMac.local>
2021-01-25 14:57:25 +0800

    修改递归

--- src/daos/KbInfoDAO.go
+++ src/daos/KbInfoDAO.go
@@ -28,6 +28,13 @@ func (this *KbInfoDAO) getKbDetail(kbName string, kbID, groupID int, result *[]*
 where kb_id = ? and pid = ? 
 order by group_order `, kbID, groupID).Find(result)
 	for _, v := range *result {
+		var grp  []*DocModel.DocGrpImpl
+		this.getKbDetail(kbName, kbID, v.GroupID,&grp)
+
+		for i, item := range grp {
+			fmt.Println(i)
+			*result=append(*result,item)
+		}
 		var docs []*DocModel.DocImpl
 		this.DB.Table("docs").Raw(`select doc_id,doc_title,shorturl from docs 
 where kb_id = ? and group_id = ? 
@@ -38,13 +45,15 @@ order by  doc_id`, kbID, v.GroupID).Find(&docs)
 			this.getKbDetail(kbName, kbID, v.GroupID, &subGrp)
 			doc.DocHref = "/" + kbName + "/" + v.GroupShortUrl + "/" + doc.DocShortUrl
 			v.Children = append(v.Children,doc)
+			var grp  []*DocModel.DocGrpImpl
+			this.getKbDetail(kbName, kbID, v.GroupID,&grp)
 
+			for i, item := range grp {
+				fmt.Println(i)
+				v.Children =append(v.Children ,item)
+			}
 		}
-		var grp []*DocModel.DocGrpImpl
-		this.getKbDetail(kbName, kbID, v.GroupID,&grp)
-		for _, item := range grp {
-			*result=append(*result,item)
-		}
+
 
 
 	}
